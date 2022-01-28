3f3d9b627a3aeaa6a5d0e0bc4dbb5fdc1a531c27
Полина Полянская <ppolina353@yandex.ru>
2022-01-28 18:40:23 +0300

    Добавьте файлы проекта.

---
+++ CompressionDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.IO;
+using System;
+using System.IO.Compression;
+
+namespace CompressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string inFilename = @"C:\!Полина\newfile.txt";
+            string outFilename = @"C:\!Полина\2.txt.gz";
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            int theByte = sourceFile.ReadByte();
+            while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte);
+                theByte = sourceFile.ReadByte();
+            }
+            compStream.Close();
+            */
+            
+            string inFilename = @"C:\!Полина\2.txt.gz";
+            string outFilename = @"C:\!Полина\suda2.txt";
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            GZipStream compStream =
+            new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte);
+                theByte = compStream.ReadByte();
+            }
+
+            compStream.Close();
+            destFile.Close();
+            sourceFile.Close();
+
+        }
+    }
+}
